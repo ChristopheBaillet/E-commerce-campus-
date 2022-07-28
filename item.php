ff7be1b6d038063529f2009870dce7c6565f49f1
Christophe BAILLET <christophe.baillet@le-campus-numerique.fr>
2022-07-28 10:27:49 +0200

    rename des variables en minuscule

--- item.php
+++ item.php
@@ -1,10 +1,10 @@
 <?php
-$Name = "Aspirateur";
-$Price = 112.25;
-$URL = "imgs/aspirateur.jpg";
+$name = "Aspirateur";
+$price = 112.25;
+$url = "imgs/aspirateur.jpg";
 ?>
 <div class="card">
-    <h1><?=$Name?></h1>
-    <img src="<?=$URL?>">
-    <p><?=$Price?>€</p>
+    <h1><?=$name?></h1>
+    <img src="<?=$url?>">
+    <p><?=$price?>€</p>
 </div>
