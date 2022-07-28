972205ce41a8320673bb360827b0bf66891f1fc8
Christophe BAILLET <christophe.baillet@le-campus-numerique.fr>
2022-07-28 10:14:08 +0200

    ajout d'une div card pour envelopper les produits

--- item.php
+++ item.php
@@ -3,7 +3,8 @@ $Name = "Aspirateur";
 $Price = 112.25;
 $URL = "imgs/aspirateur.jpg";
 ?>
-<h1><?= $Name?></h1>
-<img src="<?= $URL?>">
-<p><?= $Price?>€</p>
-
+<div class="card">
+    <h1><?=$Name?></h1>
+    <img src="<?=$URL?>">
+    <p><?=$Price?>€</p>
+</div>
