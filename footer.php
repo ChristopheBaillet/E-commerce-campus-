4371ca53ffe39169b1f6e2aca4073954fe50c21d
Christophe BAILLET <christophe.baillet@le-campus-numerique.fr>
2022-08-03 17:04:18 +0200

    tentative de footer

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
-<footer>
-    <ul>
-        <li>Réseaux Sociaux</li>
-        <li>Contact</li>
-        <li>Policy</li>
-        <li>Le campus numérique In The Alps</li>
-    </ul>
+<footer class="text-center text-white" style="background-color: #21081a;">
+    <!-- Grid container -->
+    <div class="container p-4">
+        <ul>
+            <li>truc</li>
+            <li>truc</li>
+            <li>truc</li>
+            <li>truc</li>
+        </ul>
+    </div>
+    <!-- Grid container -->
 </footer>
